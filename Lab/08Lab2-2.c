fb66e3b0d3deccb97fd57d959d2365aa9e4317fe
jinnygym <61070230@kmitl.ac.th>
2019-01-26 10:10:34 +0700

    Update Lab

---
+++ Lab/08Lab2-2.c
@@ -0,0 +1,11 @@
+/*Lab 2-2 
+By: Suphitsara Cheevanantaporn
+61070230*/
+#include <stdio.h>
+int main()
+{
+   int inputNumber;
+   scanf("%d", &inputNumber);
+   printf("The input is %d",inputNumber);
+   return 0;
+}
---
+++ Lab/09Lab2-3.c
@@ -0,0 +1,8 @@
+/*Lab 2-3 Output: "_'Hello#\%_%/#$My$#\%_%/#Friend'_"
+By: Suphitsara Cheevanantaporn 61070230*/
+#include <stdio.h>
+int main() 
+{
+    printf("\"_\'Hello#\\%%_%%/#$My$#\\%%_%%/#Friend\'_\"");
+    return 0;
+}
