b201000c5b8d42be3c2b5751aacaa3f38482dec2
jinnygym <61070230@kmitl.ac.th>
2019-02-10 17:10:12 +0700

    Unfinish Q1-Q2

---
+++ Quiz/Q2/q2-1.c
@@ -0,0 +1,9 @@
+/*Q2 By: Suphitsara Cheevanantaporn 61070230*/
+#include "stdio.h"
+int main() {
+    int i=5, j=36;
+    float x=0.001, y=0.00, z=-81.753;
+    char c='z', d='M';
+    // Assignment Statements of each question
+    return 0;
+}
